c407a7e72ef9352aa53451f7e4bfda87c2eca9a2
mikeill <mike@mzoo.org>
2022-10-04 11:51:29 -0500

    Only partially clear, by default.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose -f docker-compose.phpunit.yml down --remove-orphans
-docker image prune -af
+# docker image prune -af # Clear out everything.
 docker-compose -f docker-compose.phpunit.yml up -d
