12e58c6463ee820eb420e0e02350cd3e91501ea8
mikeill <mike@mzoo.org>
2022-10-05 10:13:30 -0500

    Keep this around for more Docker debugging.

---
+++ my-plugin/tests/test-sample.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package My_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+	/**
+	 * A single failing example test.
+	 */
+	public function test_sample_fails() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( false );
+	}
+}
