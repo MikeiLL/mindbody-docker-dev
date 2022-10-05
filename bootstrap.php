aacdd6feb6aca37df5813dfbf925412e7ea2850b
mikeill <mike@mzoo.org>
2022-10-05 11:52:35 -0500

    Don't load phpunit bootstrap file more than once.

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,13 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+# print_r("BEFORE \n");
+# print_r(\PHPUnit\Framework\Error\Deprecated::$enabled);
+# \PHPUnit\Framework\Error\Deprecated::$enabled = false;
+# print_r("AFTER \n");
+# print_r(\PHPUnit\Framework\Error\Deprecated::$enabled);
+# print_r("\n");
+
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
@@ -39,4 +46,4 @@ if (!function_exists("_manually_load_plugins")) {
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugins' );
 
 // Start up the WP testing environment.
-require "{$_tests_dir}/includes/bootstrap.php";
+require_once "{$_tests_dir}/includes/bootstrap.php";
